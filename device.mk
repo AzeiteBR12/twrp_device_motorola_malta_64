1f0c9e412c89bd47229ebea95f759f973710bda4
azeitebr12 <ferluiz082@gmail.com>
2026-07-20 14:44:23 +0000

    fix: corrige plataforma mt6765 -> mt6762 (chip real do malta_64)

--- device.mk
+++ device.mk
@@ -19,10 +19,10 @@ PRODUCT_PACKAGES += \
     android.hardware.boot@1.0-service
 
 PRODUCT_PACKAGES += \
-    bootctrl.mt6765
+    bootctrl.mt6762
 
 PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.mt6765 \
+    bootctrl.mt6762 \
     libgptutils \
     libz \
     libcutils
